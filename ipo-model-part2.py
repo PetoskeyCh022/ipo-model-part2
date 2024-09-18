0f26252d3b9279a0ba8403f07ffbfc6fe9eb426a
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2024-09-18 10:59:35 -0400

    updated ipo-niodel-part2

---
+++ ipo-model-part2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Please enter a number: "))
+num2 = float(input("Please enter another number: "))
+
+# Process
+total = num1 + num2
+
+#Output
+print("The sum of the two numbers is " + str(total) + ".")
